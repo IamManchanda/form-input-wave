c7749d1bcaf4918bbd6626eb7035612f2a9e96b6
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-07 23:33:42 +0530

    Working pass on Form Input Wave

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const labels = document.querySelectorAll(".form-control label");
+
+function iterateLabels(label) {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`,
+    )
+    .join("");
+}
+
+labels.forEach(iterateLabels);
